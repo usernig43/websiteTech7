63a3811bcca154a5e8528408ffc9c287d140d9e5
usernig43 <139851476+usernig43@users.noreply.github.com>
2023-11-10 23:22:32 +0100

    Aggiunte pagine con navbar e get method

--- tech7-backend/index.mjs
+++ tech7-backend/index.mjs
@@ -1,16 +1,23 @@
 import express from 'express';
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+//nell'ultima versione non esiste dirname quindi ho usato un'altro metodo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app=express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req,res)=>{
-    console.log("Cioa sei entrato nella home page");
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname,"public","home.html"));
 
 
 })
 
-app.listen("9000",()=>{
-    console.log("Server sta aspettando la porta 9000");
+app.listen("7000",()=>{
+    console.log("Server sta aspettando la porta 7000");
 })
 
 
